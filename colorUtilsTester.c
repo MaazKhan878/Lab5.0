dfea4dd6d6c6544440ca13a95e32f60a10f18598
Maaz_Khan <maazkhan0348810@gmail.com>
2023-11-24 23:50:41 +0500

    1 test failed due to wrong value

--- colorUtilsTester.c
+++ colorUtilsTester.c
@@ -174,7 +174,7 @@ int main(int argc, char **argv) {
     numPassed++;
   }
 
-  r = 100, g = 39, b = 40;
+  r = 100, g = 40, b = 40;
   expectedResult = 60;
   printf("TESTING: toGrayScaleAverage(%d,%d,%d): ", r, g, b);
   result = toGrayScaleAverage(r, g, b);
